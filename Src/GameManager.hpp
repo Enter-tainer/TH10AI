27c1d2a77f1bec6b79047493c5ba0bb204e55cbe
Margatroid <351652632@qq.com>
2018-07-10 21:45:37 +0800

    格式化代码并加入对坐标系的注释

--- Src/GameManager.hpp
+++ Src/GameManager.hpp
@@ -3,59 +3,80 @@
 #include <map>
 #include <queue>
 #include "GameConnection.hpp"
+//--------游戏坐标系示意图----------
+//(-200,0)        (0,0)      (200,0)
+//   +-----------+-----------+-->
+//   |           |           |  x
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           |           |
+//   |           | (0,480)   |
+//   +-----------+-----------+
+//               v
+//              y
 
 const double s2d2 = sqrt(2.0) / 2.0;
-const double dx[9] = {0, 1.0, s2d2, 0, -s2d2, -1.0, -s2d2, 0, s2d2};
-const double dy[9] = {0, 0, -s2d2, -1.0, -s2d2, 0, s2d2, 1.0, s2d2};
-const double playerSpeed[2] = {4.5, 2.0};
+const double dx[9] = { 0, 1.0, s2d2, 0, -s2d2, -1.0, -s2d2, 0, s2d2 };
+const double dy[9] = { 0, 0, -s2d2, -1.0, -s2d2, 0, s2d2, 1.0, s2d2 };
+const double playerSpeed[2] = { 4.5, 2.0 }; // (pixel / frame)
 const Vec2d ulCorner = Vec2d(-200, 0);
 const Vec2d drCorner = Vec2d(200, 480);
 const double eps = 1e-7;
 //从高位到低位分别为上下左右
-const int keyinfo[9] = {0x0, 0x1, 0x9, 0x8, 0xa, 0x2, 0x6, 0x4, 0x5};
+const int keyinfo[9] = { 0x0, 0x1, 0x9, 0x8, 0xa, 0x2, 0x6, 0x4, 0x5 };
 enum class GameState {
-    NORMAL,
-    COLLECT,
-    MOVE
+	NORMAL,
+	COLLECT,
+	MOVE
 };
 
 struct Node {
-    int8_t time;
-    Vec2d pos;
-    constexpr Node(int8_t time_, Vec2d pos_) noexcept : time(time_), pos(pos_) {}
+	int8_t time;
+	Vec2d pos;
+	constexpr Node(int8_t time_, Vec2d pos_) noexcept : time(time_), pos(pos_) {}
 };
 
 bool operator<(const Node& lhs, const Node& rhs);
 
 struct NodeSave {
-    bool shift;
-    int from;
-    double value;
-    NodeSave() = default;
-    constexpr NodeSave(int from_, bool shift_, double value_) noexcept : shift(shift_), from(from_), value(value_) {}
+	bool shift;
+	int from;
+	double value;
+	NodeSave() = default;
+	constexpr NodeSave(int from_, bool shift_, double value_) noexcept : shift(shift_), from(from_), value(value_) {}
 };
 
 
 class GameManager {
 public:
-    GameManager() : mState(GameState::NORMAL), mConnection(createGameConnection()) {}
-    void update(unsigned long long frameCount);
+	GameManager() : mState(GameState::NORMAL), mConnection(createGameConnection()) {}
+	void update(unsigned long long frameCount);
 private:
-    std::map<Node, NodeSave> valueMap;
-    std::queue<Node> bfsQueue;
-    GameState mState;
-    Player mPlayer {};
-    std::vector<Object> mEnemy;
-    std::vector<Object> mBullet;
-    std::vector<Laser> mLaser;
-    std::vector<Object> mPowers;
-    std::unique_ptr<GameConnection> mConnection;
-    bool legalState(Node state) const noexcept;
-    //估价效率过低，待修改
-    double getValue(Node state) const noexcept;
-    static Vec2d fixupPos(const Vec2d& pos) noexcept;
-    static bool hitTest(const Object& a, const Object& b) noexcept;
-    static bool hitTestBombChoice(const Object& a, const Object& b) noexcept;
-    //地图位置估价
-    static double getMapValue(Vec2d pos) noexcept;
+	std::map<Node, NodeSave> valueMap;
+	std::queue<Node> bfsQueue;
+	GameState mState;
+	Player mPlayer{};
+	std::vector<Object> mEnemy;
+	std::vector<Object> mBullet;
+	std::vector<Laser> mLaser;
+	std::vector<Object> mPowers;
+	std::unique_ptr<GameConnection> mConnection;
+	bool legalState(Node state) const noexcept;
+	//估价效率过低，待修改
+	double getValue(Node state) const noexcept;
+	static Vec2d fixupPos(const Vec2d& pos) noexcept;
+	static bool hitTest(const Object& a, const Object& b) noexcept;
+	static bool hitTestBombChoice(const Object& a, const Object& b) noexcept;
+	//地图位置估价
+	static double getMapValue(Vec2d pos) noexcept;
 };
